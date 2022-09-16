fe451fd421ab0d3b4f65591b09d81b45c1143f11
Hooman <111180271+HoomanNR@users.noreply.github.com>
2022-09-16 22:20:32 +0430

    Add files via upload

---
+++ Parallelogram/Area.py
@@ -0,0 +1,15 @@
+#Greetings
+print("Hi there")
+
+#Getting Information
+Base = input("Enter the parallelogram base : ")
+Base = float(Base)
+Height = input("Enter the parallelogram height : ")
+Height = float(Height)
+
+#Giving Information
+print("")
+Area = Base * Height
+Area = round(Area , 3)
+print("The parallelogram Area is = " , Area )
+print("Have a nice day ...")
---
+++ Parallelogram/Perimeter.py
@@ -0,0 +1,15 @@
+#Greetings
+print("Hi there")
+
+#Gettings Information
+side1 = input("Enter one side of parallelogram : ")
+side1 = float(side1)
+side2 = input("Please enter another side of parallelogram : ")
+side2 = float(side2)
+
+#Giving Information
+print("")
+Perimeter = (side1 + side2 ) * 2
+Perimeter = round(Perimeter , 3)
+print("The parallelogram perimeter is = " , Perimeter)
+print("Have a nice day ...")
