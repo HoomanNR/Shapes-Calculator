2c41727bb10e5ad04e9ffbc822f6b45de40d14d0
Hooman <111180271+HoomanNR@users.noreply.github.com>
2022-09-16 22:21:32 +0430

    Add files via upload

---
+++ Rectangle/Area.py
@@ -0,0 +1,15 @@
+#Grettings
+print("Hi there")
+
+#Getting Information
+Length = input("Enter the rectangle length : ")
+Length = float(Length)
+Width = input("Enter the rectangle widgth : ")
+Width = float(Width)
+
+#Giving Information
+print("")
+Area = Length * Width
+Area = round(Area , 3)
+print("The rectangle area is = " , Area)
+print("Have a nice day ...")
---
+++ Rectangle/Perimeter.py
@@ -0,0 +1,15 @@
+#Grettings
+print("Hi there")
+
+#Getting Information
+Length = input("Enter the rectangle length : ")
+Length = float(Length)
+Width = input("Enter the rectangle widgth : ")
+Width = float(Width)
+
+#Giving Information
+print("")
+Perimeter = (Length + Width) * 2
+Perimeter = round(Perimeter , 3)
+print("The rectangle perimeter is = " , Perimeter)
+print("Have a nice day ...")
