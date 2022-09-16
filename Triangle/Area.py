3b563fdcdebe7c2b7acbc1489600bc5248dae30a
Hooman <111180271+HoomanNR@users.noreply.github.com>
2022-09-16 22:24:42 +0430

    Add files via upload

---
+++ Triangle/Area.py
@@ -0,0 +1,15 @@
+#Greetings
+print("Hi there")
+
+#Getting Information
+Base = input("Enter the triangle base : ")
+Base = float(Base)
+Height = input("Enter the triangle height : ")
+Height = float(Height)
+
+#Giving Information
+print("")
+Area = ( Base * Height ) / 2
+Area = round(Area , 3)
+print("The triangle area is = " , Area)
+print("Have a nice day ...")
---
+++ Triangle/Perimeter.py
@@ -0,0 +1,17 @@
+#Greetings
+print("Hi there")
+
+#Getting Information
+side1 = input("Please enter the first side of triangle : ")
+side1 = float(side1)
+side2 = input("Please enter the second side of triangle : ")
+side2 = float(side2)
+side3 = input("Please enter the third side of triangle : ")
+side3 = float(side3)
+
+#Giving the answers
+print("")
+Perimeter = side1 + side2 + side3
+Perimeter = round(Perimeter)
+print("The triangle perimeter is = " , Perimeter)
+print("Have a nice day ...")
