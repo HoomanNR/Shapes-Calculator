96e8d8888bf4f47f12fbb561aa9fc6c8159025eb
Hooman <111180271+HoomanNR@users.noreply.github.com>
2022-09-16 21:23:03 +0430

    Add files via upload

---
+++ Area.py
@@ -0,0 +1,13 @@
+#Greetings
+print("Hi there")
+
+#Getting Information
+Radius = input("Enter the circle radius : ")
+Radius = float(Radius)
+
+#Giving Information
+print("")
+Area = Radius * Radius * 3.14
+Area = round(Area , 3)
+print("The circle area is = " , Area)
+print("Have a nice day ...")
---
+++ Perimeter.py
@@ -0,0 +1,13 @@
+#Greetings
+print("Hi there")
+
+#Getting Information
+Diameter = input("Enter the circle diameter : ")
+Diameter = float(Diameter)
+
+#Giving Information
+print("")
+Perimeter = Diameter * 3.14
+Perimeter = round(Perimeter , 3)
+print("The circle perimeter is = " , Perimeter)
+print("Have a nice day ...")
