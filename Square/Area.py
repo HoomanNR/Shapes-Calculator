4f085fd07b2f1c8ce21ecd091e28084ac743ad59
Hooman <111180271+HoomanNR@users.noreply.github.com>
2022-09-16 22:22:33 +0430

    Add files via upload

---
+++ Square/Area.py
@@ -0,0 +1,13 @@
+#Greetings 
+print("Hi there")
+
+#Getting Information
+Side = input("Enter the square side : ")
+Side = float(Side)
+
+#Giving Information
+print("")
+Area = Side * Side
+Area = round(Area , 3)
+print("The square area is = " , Area)
+print("Have a nice day ...")
---
+++ Square/Perimeter.py
@@ -0,0 +1,13 @@
+#Greetings 
+print("Hi there")
+
+#Getting Information
+Side = input("Enter the square side : ")
+Side = float(Side)
+
+#Giving Information
+print("")
+Perimeter = Side * 2
+Perimeter = round(Perimeter , 3)
+print("The square Perimeter is = " , Perimeter)
+print("Have a nice day ...")
